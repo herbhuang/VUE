f33f60aae8588caedd44c7a610726502999e606e
Ryan Pavlik <ryan.pavlik@collabora.com>
2020-11-20 12:35:31 -0600

    Clean up resources

--- .github/workflows/make-snapshot-version.sh
+++ .github/workflows/make-snapshot-version.sh
@@ -3,5 +3,4 @@ set -e
 
 NEWVER=+CI-$(date +%Y%m%d)
 echo "Appending suffix snapshot version $NEWVER"
-sed -i "s/info.VUEVersion  =\(.*\)/\1$NEWVER/" VUE2/src/main/resources/tufts/vue/VueResources.properties
 sed -i "s/vue.version=\(.*\)/\1$NEWVER/" VUE2/src/main/resources/tufts/vue/VueResources.properties
